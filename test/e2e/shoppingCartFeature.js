365e607a30b73ed76b87c6ee4e1a709215ba5bde
wardymate <chriswarddev@gmail.com>
2015-05-19 08:51:56 +0100

    added protractor elements for end to end testing

---
+++ test/e2e/shoppingCartFeature.js
@@ -0,0 +1,6 @@
+describe('Shopping Cart', function() {
+  it('has a title', function() {
+    browser.get('http://localhost:8080');
+    expect(browser.getTitle()).toEqual('Clothes Shop');
+  });
+});
