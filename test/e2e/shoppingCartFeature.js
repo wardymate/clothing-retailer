e680727f4f7d29e9ced18878b7f29411f92448ea
wardymate <chriswarddev@gmail.com>
2015-05-21 16:50:41 +0100

    feature test adding item to the basket passing

--- test/e2e/shoppingCartFeature.js
+++ test/e2e/shoppingCartFeature.js
@@ -8,12 +8,12 @@ describe('Shopping Cart', function() {
   });
 
   it('has a hidden shopping basket', function() {
-    expect(browser.isElementPresent(by.id('shopping-basket'))).toBe(false);
+    expect(element(by.id('shopping-basket')).isDisplayed()).toBe(false);
     expect(element(by.id('basket-header-total')).getText()).toContain('£0.00');
   });
 
   it('can add an item to the shopping cart', function() {
-    element(by.cssContainingText('.list-group', 'Suede Shoes : Blue')).click();
+    element(by.cssContainingText('.basket-item', 'Suede Shoes : Blue')).click();
     expect(element(by.id('basket-header-total')).getText()).toContain('£42.00');
     expect(element(by.id('shopping-basket')).getText()).toContain('Suede Shoes : Blue');
   });
